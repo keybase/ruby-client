a8d63f95150f00c60899715cfecdd1ae28ec25fc
Sean Handley <sean.handley@gmail.com>
2014-03-16 19:53:31 +0000

    Handle keybase errors.

---
+++ lib/keybase/error.rb
@@ -0,0 +1,25 @@
+module Keybase
+  class Error
+    def self.raise_unless_successful(status)
+      return if status['code'] == 0
+      message = status['fields'] ? error_with_fields(status) : status['desc']
+      raise Keybase::errors[status['code']], message
+    end
+    
+    def self.error_with_fields(status)
+      "#{status['desc']}. #{status['fields'].map{|k,v| "#{k.to_s}: #{v}"}}"
+    end
+  end
+  
+  class UserNotFoundError < StandardError; end
+  class InputError < StandardError; end
+  
+  private
+    
+  def self.errors
+    {
+      100 => InputError,
+      205 => UserNotFoundError
+    }
+  end 
+end
