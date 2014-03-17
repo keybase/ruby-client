d1ed97b0bd837a1c5c6b51b29566b9adc7b2fb81
Sean Handley <sean.handley@gmail.com>
2014-03-17 15:10:28 +0000

    Rewritten with minitest stubs.

--- test/unit/lib/keybase/response_test.rb
+++ test/unit/lib/keybase/response_test.rb
@@ -3,18 +3,20 @@ module Keybase
   
   class ResponseTest < Minitest::Test
     def setup
-      @response = Object.new
-      @response.stubs(body: {'status' => {'code' => 0, 'desc' => 'all fine'}}.to_json)
+      @response = OpenStruct.new(body: nil)
     end
     
     def test_does_not_raise
-      Response.new(@response)  
+      @response.stub :body, {'status' => {'code' => 0, 'desc' => 'all fine'}}.to_json do
+        Response.new(@response)  
+      end
     end
     
     def test_raises_user_not_found
-      @response.stubs(body: {'status' => {'code' => 205, 'desc' => 'User not found'}}.to_json)
-      assert_raises(Keybase::UserNotFoundError, 'User not found') do
-        Response.new(@response) 
+      @response.stub :body, {'status' => {'code' => 205, 'desc' => 'User not found'}}.to_json do
+        assert_raises(Keybase::UserNotFoundError, 'User not found') do
+          Response.new(@response) 
+        end
       end
     end
   end
