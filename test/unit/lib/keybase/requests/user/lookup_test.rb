805d7eb8349c77e9c554f27201ba9b60ad7d61f2
Sean Handley <sean.handley@gmail.com>
2014-03-23 11:08:47 +0000

    Test that lookup returns 'them' contents from response hash.

--- test/unit/lib/keybase/requests/user/lookup_test.rb
+++ test/unit/lib/keybase/requests/user/lookup_test.rb
@@ -3,6 +3,15 @@ require_relative '../../../../../test_helper'
 module Keybase
   module Request
     class LookupTest < Minitest::Test
+      def setup
+        @response = { 'them' => true }
+      end
+
+      def test_lookup_returns_them
+        Base.stub :get, @response do
+          assert_equal true, User.lookup('foo') # returns true
+        end
+      end
     end
   end
 end
