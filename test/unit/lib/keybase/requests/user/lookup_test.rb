ebc24e6916be7a47fdabfcf7391edd04afbf1ca3
Sean Handley <sean.handley@gmail.com>
2014-03-23 14:07:01 +0000

    Use example user in this test.

--- test/unit/lib/keybase/requests/user/lookup_test.rb
+++ test/unit/lib/keybase/requests/user/lookup_test.rb
@@ -4,12 +4,12 @@ module Keybase
   module Request
     class LookupTest < Minitest::Test
       def setup
-        @response = { 'them' => true }
+        @response = { 'them' => EXAMPLE_USER }
       end
 
       def test_lookup_returns_them
         Base.stub :get, @response do
-          assert_equal true, User.lookup('foo') # returns true
+          assert_equal 'bf5f1650fc1361a', User.lookup('foo')['id']
         end
       end
     end
