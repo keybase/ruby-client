77f02df0763c699fd83187815e98a4467d3b15e4
Sean Handley <sean.handley@gmail.com>
2014-03-23 09:21:09 +0000

    Test user lookup on model.

--- test/unit/lib/keybase/models/user_test.rb
+++ test/unit/lib/keybase/models/user_test.rb
@@ -1,6 +1,18 @@
-require_relative '../../../../test_helper'
-
 module Keybase
+  
+  require_relative '../../../../test_helper'
+  
   class UserTest < Minitest::Test
+    def setup
+      @user_hash = {'id' => 1}
+    end
+
+    def test_user_lookup_loads_from_returned_hash
+      Request::User.stub :lookup, @user_hash do
+        user = User.lookup('foo')
+        assert_equal @user_hash['id'], user.id
+      end
+    end
+    
   end
 end
