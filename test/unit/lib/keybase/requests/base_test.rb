2079df2d50b61e7ecaffeefd7f50cc119b0c3fc3
Sean Handley <sean.handley@gmail.com>
2014-03-23 10:38:12 +0000

    Base Request object tests.

--- test/unit/lib/keybase/requests/base_test.rb
+++ test/unit/lib/keybase/requests/base_test.rb
@@ -3,6 +3,26 @@ module Keybase
   
   module Request
     class RequestBaseTest < Minitest::Test
+      def setup
+        @server_response = Object.new
+        @conn = Faraday.new
+        @response = OpenStruct.new(body: true)
+      end
+
+      def test_get_returns_keybase_response
+        Base.stub :conn, @conn do
+          @conn.stub :get, nil do
+            Keybase::Response.stub :new, @response do
+              assert Base.get('foo', {})
+            end
+          end
+        end
+      end
+      
+      def test_base_url_defined
+        assert defined?(API_BASE_URL)
+      end
+      
     end
   end
 end
