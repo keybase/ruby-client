b53ea535412029386b5556472ba9aa4b6a3c366e
Sean Handley <sean.handley@gmail.com>
2014-03-23 14:06:13 +0000

    Load a decent user fixture.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 SimpleCov.start if require 'simplecov'
 
 require_relative '../lib/keybase'
+EXAMPLE_USER = JSON.parse(IO.read(File.expand_path('../fixtures/example_user.json', __FILE__)))
 
 require 'rubygems'
 gem "minitest"
