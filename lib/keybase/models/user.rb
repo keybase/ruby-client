ca319aaec912fd85cc465a014e10fb5496788252
Sean Handley <sean.handley@gmail.com>
2014-03-23 14:06:49 +0000

    Populate user object with returned data.

--- lib/keybase/models/user.rb
+++ lib/keybase/models/user.rb
@@ -1,14 +1,61 @@
 module Keybase
   class User
     
-    attr_reader :id
+    attr_reader :id, :basics, :invitation_stats, :profile, :emails,
+                :public_keys, :private_keys
 
     def initialize(params)
       @id = params['id']
+      @invitation_stats = OpenStruct.new(params['invitation_stats'])
+      
+      set_basics(params['basics'])             if params['basics']
+      set_profile(params['profile'])           if params['profile']
+      set_emails(params['emails'])             if params['emails']
+      set_public_keys(params['public_keys'])   if params['public_keys']
+      set_private_keys(params['private_keys']) if params['private_keys']
     end
     
     def self.lookup(username)
       new(Request::User.lookup(username))
+    end
+    
+    private
+    
+    def set_basics(params)
+      @basics = OpenStruct.new(params.merge(created_at: nil, updated_at: nil))
+      @basics.created_at = Time.at(@basics.ctime).to_datetime if @basics.ctime
+      @basics.updated_at = Time.at(@basics.mtime).to_datetime if @basics.mtime
+    end
+    
+    def set_profile(params)
+      @profile = OpenStruct.new(params.merge(updated_at: nil))
+      @profile.updated_at = Time.at(@profile.mtime).to_datetime if @profile.mtime
+    end
+
+    def set_emails(params)
+      @emails = OpenStruct.new(params)
+      params.each do |k,v|
+        v.merge!('is_verified?' => (v['is_verified'] == 1 ? true : false))
+        @emails.send("#{k}=".to_sym, OpenStruct.new(v))
+      end
+    end
+    
+    def set_public_keys(params)
+      @public_keys = OpenStruct.new(params)
+      params.each do |k,v|
+        v.merge!('created_at' => Time.at(v['ctime']).to_datetime)
+        v.merge!('updated_at' => Time.at(v['mtime']).to_datetime)
+        @public_keys.send("#{k}=".to_sym, OpenStruct.new(v))
+      end      
+    end
+    
+    def set_private_keys(params)
+      @private_keys = OpenStruct.new(params)
+      params.each do |k,v|
+        v.merge!('created_at' => Time.at(v['ctime']).to_datetime)
+        v.merge!('updated_at' => Time.at(v['mtime']).to_datetime)
+        @private_keys.send("#{k}=".to_sym, OpenStruct.new(v))
+      end      
     end
   end
 end
--- test/unit/lib/keybase/models/user_test.rb
+++ test/unit/lib/keybase/models/user_test.rb
@@ -4,14 +4,56 @@ module Keybase
   
   class UserTest < Minitest::Test
     def setup
-      @user_hash = {'id' => 1}
+      @user = User.new(EXAMPLE_USER)
+    end
+    
+    def test_unix_times_parsed_to_dates_for_basics
+      assert_equal "2013-10-30T19:24:21+00:00",
+                    @user.basics.created_at.to_s
+      assert_equal "2013-10-30T19:41:01+00:00",
+                    @user.basics.updated_at.to_s
+    end
+    
+    def test_unix_times_parsed_to_dates_for_profile
+      assert_equal "2013-10-31T19:22:19+00:00",
+                   @user.profile.updated_at.to_s      
+    end
+
+    def test_unix_times_parsed_to_dates_for_public_keys
+      assert_equal "2013-11-05T21:00:12+00:00",
+                   @user.public_keys.primary.created_at.to_s
+      assert_equal "2013-11-05T21:00:12+00:00",
+                   @user.public_keys.primary.updated_at.to_s
+    end
+        
+    def test_unix_times_parsed_to_dates_for_private_keys
+      assert_equal "2013-11-05T21:00:12+00:00",
+                   @user.private_keys.primary.created_at.to_s
+      assert_equal "2013-11-05T21:00:12+00:00",
+                   @user.private_keys.primary.updated_at.to_s
+    end
+    
+    def test_emails_set_correctly
+      assert_equal EXAMPLE_USER['emails']['primary']['email'], @user.emails.primary.email 
+      assert_equal true, @user.emails.primary.is_verified?
+      assert_equal EXAMPLE_USER['emails']['secondary']['email'], @user.emails.secondary.email
+      assert_equal false, @user.emails.secondary.is_verified?
     end
 
     def test_user_lookup_loads_from_returned_hash
-      Request::User.stub :lookup, @user_hash do
+      Request::User.stub :lookup, EXAMPLE_USER do
         user = User.lookup('foo')
-        assert_equal @user_hash['id'], user.id
+        assert_equal EXAMPLE_USER['id'], user.id
       end
+    end
+    
+    def test_object_loads_when_data_missing
+      @empty = User.new({})
+      assert_nil @empty.basics
+      assert_nil @empty.profile
+      assert_nil @empty.emails
+      assert_nil @empty.public_keys
+      assert_nil @empty.private_keys
     end
     
   end
