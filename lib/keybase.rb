4afb02dcc9373e0622cce96e54365ad243f475cd
Sean Handley <sean.handley@gmail.com>
2014-03-24 22:10:13 +0000

    Include files and add dump methods to namespace.

--- lib/keybase.rb
+++ lib/keybase.rb
@@ -7,9 +7,10 @@ require 'base64'
 require 'faraday-cookie_jar'
 
 module Keybase 
-  require_relative 'keybase/token_store'
+  require_relative 'keybase/crypto'
   require_relative 'keybase/error'
   require_relative 'keybase/models/user'
+  require_relative 'keybase/models/dump'
   require_relative 'keybase/request/base'
   require_relative 'keybase/request/dump/all'
   require_relative 'keybase/request/dump/latest'
@@ -20,8 +21,17 @@ module Keybase
   require_relative 'keybase/request/sig/post_auth'
   require_relative 'keybase/request/user/lookup'
   require_relative 'keybase/response'
+  require_relative 'keybase/token_store'
   
   def self.lookup(username)
     User.lookup(username)
+  end
+  
+  def self.dump_all
+    Dump.all
+  end
+  
+  def self.dump_latest
+    Dump.latest
   end
 end
