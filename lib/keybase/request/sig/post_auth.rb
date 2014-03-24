ab0e1c82d199cc969578d5fce051263ca52e7a8e
Sean Handley <sean.handley@gmail.com>
2014-03-24 22:28:12 +0000

    Extract the auth token from response.

--- lib/keybase/request/sig/post_auth.rb
+++ lib/keybase/request/sig/post_auth.rb
@@ -5,7 +5,8 @@ module Keybase
   module Request
     class Sig < Base
       def self.post_auth(email_or_username, sig)
-        post('sig/post_auth.json', email_or_username: email_or_username, sig: sig)
+        post('sig/post_auth.json', email_or_username: email_or_username,
+                                   sig: sig)['auth_token']
       end
     end
   end
