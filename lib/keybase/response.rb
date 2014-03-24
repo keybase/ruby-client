a3ffeacfa6c95a6f7bc851434c3d72db061a5615
Sean Handley <sean.handley@gmail.com>
2014-03-24 22:09:03 +0000

    Don't dump body - can use Faraday to debug.

--- lib/keybase/response.rb
+++ lib/keybase/response.rb
@@ -5,14 +5,8 @@ module Keybase
     
     def initialize(server_response)
       @body = JSON.parse(server_response.body)
+      Error.raise_unless_successful(@body['status'])
       @cookie = server_response.headers['Set-Cookie'] ? server_response.headers['Set-Cookie'] : nil
-      begin
-        Error.raise_unless_successful(@body['status'])
-      rescue
-        STDERR.puts @body unless defined?(TEST_ENV)
-        raise
-      end
-      @body
     end
     
   end
