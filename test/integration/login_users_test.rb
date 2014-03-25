75b3e1de09b75337bc76ed04c72ce0fbcd753eb7
Sean Handley <sean.handley@gmail.com>
2014-03-25 13:20:39 +0000

    Update contents.

--- test/integration/login_users_test.rb
+++ test/integration/login_users_test.rb
@@ -5,12 +5,12 @@ module Keybase
   class LoginUsersIntegrationTest < Minitest::Test
     
     def test_login
-      VCR.use_cassette('user_login_foo') do
+      VCR.use_cassette('user_login') do
         user = User.login('seanhandley', 'secret')
         assert_equal "secret", user.private_keys.primary.bundle
         assert_equal "dude@testing.com", user.emails.primary.email
-        assert_equal 'lgHZIGI3OTQ0MzYxZDI0NzdmMmM4YTlkNzJjOWVkZWNlNzA4zlMvTE/OAAFRgMDEIJ6M5LAAPzkqRi9BLRImwFV0v9Nh0tcL1Whuf1y5ZyE+', TokenStore.csrf
-        assert_equal 'guest=lgHZIDk4OTFhMWY4NTg3ZmM4NWQxMmUzOTlkMzJlZDk4NDA4zlMvTFDOAAFRgMDEIIR3zxU0IOIdPEJDrCDC9jLHqPxB%2Bg8qGhx1NdtAg4kH; Max-Age=604.8; Domain=.keybase.io; Path=/; Expires=Sun, 23 Mar 2014 21:14:21 GMT; HttpOnly; Secure, session=lgHZIGU3MjBmNWE0Y2NlOWM2MGY5NDUzOGQzMDk1OWU3MTAwzlMvTFDOAeEzgNkgYTZjZmJmYjg0MWIzOGRlODk4MzZlYzY3ODZiMWFjMDLEIGOPTA3Memn2DZjT9OnMkCEQ9d7bEF5R29HbDrXwRz11; Max-Age=31536000; Domain=.keybase.io; Path=/; Expires=Mon, 23 Mar 2015 21:04:16 GMT; HttpOnly; Secure', TokenStore.cookie
+        assert_equal 'lgHZIDA0MmNjYWYwMTMxZmZlZDNiZGE1MDAyM2M3ZGEzNzA4zlMxfzfOAAFRgMDEIPBYxx9HWUnY8GVK2rYzx29hOQW5DXDNkONntT1FjGxI', TokenStore.csrf
+        assert_equal 'guest=lgHZIGJkM2E1MGNmMjBjNmQ0MzQwMmFhMDI4Mjc4YzJiZjA4zlMxfzjOAAFRgMDEINQblgb3pKa15JV58bI2VqKfWGvPFe3qcLUuq153%2Be7C; Max-Age=604.8; Domain=.keybase.io; Path=/; Expires=Tue, 25 Mar 2014 13:16:04 GMT; HttpOnly; Secure, session=lgHZIGU3MjBmNWE0Y2NlOWM2MGY5NDUzOGQzMDk1OWU3MTAwzlMxfzjOAeEzgNkgODViZmU1ZGI4MzM3NzM4NDk2MDRmZGIzOWZkM2UxMDLEII%2B6%2FmdEgDo74uwb%2BQDMrDt6m06NFhs0eQjX9popcagO; Max-Age=31536000; Domain=.keybase.io; Path=/; Expires=Wed, 25 Mar 2015 13:06:00 GMT; HttpOnly; Secure', TokenStore.cookie
       end
     end
 
