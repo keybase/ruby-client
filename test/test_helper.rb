c8651a3e1e14ca28da7b384363073ef9f0e76d49
Sean Handley <sean.handley@gmail.com>
2014-03-25 08:40:21 +0000

    Test the dump model.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ SimpleCov.start if require 'simplecov'
 
 require_relative '../lib/keybase'
 EXAMPLE_USER = JSON.parse(IO.read(File.expand_path('../fixtures/example_user.json', __FILE__)))
+EXAMPLE_DUMP = JSON.parse(IO.read(File.expand_path('../fixtures/example_dump.json', __FILE__)))
+
 TEST_ENV = true
 
 require 'rubygems'
