95586f919a685581ec0cad8566ccd5604375010c
Sean Handley <sean.handley@gmail.com>
2014-03-25 12:18:55 +0000

    Pack and decode should be crypto's responsibility.

--- lib/keybase/crypto.rb
+++ lib/keybase/crypto.rb
@@ -2,14 +2,18 @@ module Keybase
   class Crypto
     SCRYPT_VARS = {'N' => 2**15, 'r' => 8, 'p' => 1, 'key_length' => 224, 'chunk' => 192..-1}
     def self.scrypt(passphrase, salt)
-      SCrypt::Engine.scrypt(passphrase, salt,
+      SCrypt::Engine.scrypt(passphrase, hex2bin(salt),
                                    SCRYPT_VARS['N'], SCRYPT_VARS['r'],
                                    SCRYPT_VARS['p'], SCRYPT_VARS['key_length']
                                  )[SCRYPT_VARS['chunk']]
     end
     
     def self.hmac_sha512(key, data)
-      OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA512.new, key, data)      
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA512.new, key, Base64.decode64(data))
+    end
+    
+    def self.hex2bin(hex)
+      [hex].pack("H*")
     end
   end
 end
